86d979d9e67add2d3a0c3cbb1e160125c199519d
ledwindra <ledwindra@users.noreply.github.com>
2021-01-09 23:09:38 +0700

    added stuffs

--- nber_paper.py
+++ nber_paper.py
@@ -1,3 +1,4 @@
+import json
 import matplotlib.font_manager as font_manager
 import matplotlib.pyplot as plt
 import numpy as np
@@ -7,6 +8,7 @@ import requests
 import seaborn as sns
 import statistics
 import warnings
+from glob import glob
 
 # set xkcd mode for plotting
 font_manager._rebuild()
@@ -78,65 +80,59 @@ def collaboration(df):
     # transform data
     df = df[df['citation_publication_date'].isna() == False]
     df = df[['citation_publication_date', 'citation_author']]
-    df['citation_author'] = df.apply(lambda x: re.sub('[^A-Za-z0-9,\' ]+', '', x['citation_author']), axis=1)
-    df['citation_author'] = df.apply(lambda x: x['citation_author'].split('\''), axis=1)
-    df['citation_author'] = df.apply(lambda x: [x for x in x['citation_author'] if x != '' and x != ', '], axis=1)
     df['total_author'] = df.apply(lambda x: len(x['citation_author']), axis=1)
-    df['year'] = df.apply(lambda x: int(x['citation_publication_date'][:4]), axis=1)
+    df['year'] = df.apply(lambda x: x['citation_publication_date'][:4], axis=1)
+    df = df[df['year'] != 'None']
+    df['year'] = df['year'].astype(int)
     df = df.groupby('year')['total_author'].agg('median')
     df = df.to_frame().reset_index()
     df = df.rename(columns={'total_author': 'median_total_author'})
     
     # visualize data
-    min_year = min(df['year'])
-    min_year = int(str(min_year).replace(str(min_year)[-1], '0'))
-    max_year = max(df['year'])
     fig, ax = plt.subplots(figsize=(15, 5))
     plt.plot('year', 'median_total_author', data=df, color='black')
     plt.title(f'Median total authors per paper each year')
     plt.xlabel('Year')
     plt.ylabel('Median total author')
-    plt.xticks(np.arange(min_year, max_year+1, 3))
+    plt.xticks(np.arange(df.year.min(), df.year.max()+1, 3))
     plt.yticks(np.arange(min(df['median_total_author']), max(df['median_total_author'])+1, 1))
     plt.show
     
 def count_by_year(df):
     df = df[df['citation_publication_date'].isna() == False]
     count = df[['id', 'citation_publication_date']]
-    count['citation_publication_date'] = count.apply(lambda x: int(x['citation_publication_date'][:4]), axis=1)
+    count['citation_publication_date'] = count.apply(lambda x: x['citation_publication_date'][:4], axis=1)
     count = count.groupby('citation_publication_date').size().to_frame().reset_index()
     count = count.rename(columns={0: 'count', 'citation_publication_date': 'year'})
     
     return count
     
 def get_data(file_name):
-    df = pd.read_csv(file_name)
-    df = df[~df['id'].isna() == True]
-    df['id'] = df['id'].astype(int)
+    df = glob(f'data/{file_name}/*')
+    df = [json.load(open(x, 'r')) for x in df]
+    df = pd.DataFrame(df)
     
     return df
 
 def published_paper(df, title, x_interval=1, y_interval=1):
+    df = df[df['year'] != 'None']
+    df['year'] = df['year'].astype(int)
     fig, ax = plt.subplots(figsize=(15, 5))
     plt.plot('year', 'count', data=df, color='black')
     plt.title(title)
     plt.xlabel('Year')
     plt.ylabel('Total paper')
-    min_year = min(df['year'])
-    min_year = int(str(min_year).replace(str(min_year)[-1], '0'))
-    plt.xticks(np.arange(min_year, max(df['year'])+1, x_interval))
-    plt.yticks(np.arange(0, max(df['count'])+1, y_interval))
+    plt.xticks(np.arange(df.year.min(), df.year.max()+1, x_interval))
+    plt.yticks(np.arange(0, max(df['count']), y_interval))
     plt.show
     
 def section_trends(df, column, title, keyword, x_interval, y_interval):
     # transform data
-    df = df[df['citation_publication_date'].isna() == False]
     df = df[['citation_publication_date', column]]
     df[column] = df[column].fillna('N/A')
-    df.loc[:, 'year'] = df.apply(lambda x: int(x['citation_publication_date'][:4]), axis=1)
-    min_year = min(df['year'])
-    min_year = int(str(min_year).replace(str(min_year)[-1], '0'))
-    max_year = max(df['year'])
+    df.loc[:, 'year'] = df.apply(lambda x: x['citation_publication_date'][:4], axis=1)
+    df = df[df['year'] != 'None']
+    df['year'] = df['year'].astype(int)
     df.loc[:, column] = df.apply(lambda x: x[column].lower(), axis=1)
     df = df[df.apply(lambda x: keyword in x[column], axis=1) == True]
     df = df.groupby('year').size().to_frame().reset_index()
@@ -148,18 +144,13 @@ def section_trends(df, column, title, keyword, x_interval, y_interval):
     plt.title(f'NBER trends: {title} in the {column} section in NBER')
     plt.xlabel('Year')
     plt.ylabel('Total paper')
-    plt.xticks(np.arange(min_year, max_year+1, x_interval))
-    plt.yticks(np.arange(0, max(df['count'])+1, y_interval))
+    plt.xticks(np.arange(df.year.min(), df.year.max()+1, x_interval))
+    plt.yticks(np.arange(0, max(df['count']), y_interval))
     plt.show
     
-def top_five(df, column='topics', split_by=','):
+def top_five(df, column='topics'):
     df = df[df[column].isna() == False]
     df = df[[column]]
-    if column == 'citation_author':
-        df.loc[:, column] = df.apply(lambda x: re.sub('[^A-Za-z0-9,\' ]+', '', x[column]), axis=1)
-    else:
-        df.loc[:, column] = df.apply(lambda x: re.sub('[^A-Za-z0-9 ]+', '', x[column]), axis=1)
-    df.loc[:, column] = df.apply(lambda x: x[column].split(split_by), axis=1)
     df = df.explode(column)
     df = df.groupby(column).size().to_frame().reset_index()
     df = df.rename(columns={0: 'count'})
@@ -170,26 +161,30 @@ def top_five(df, column='topics', split_by=','):
     
     return df
 
-def top_five_trends(df, column, split_by, title):
+def top_five_trends(df, column, title):
     # transform
-    df = df[df[column].isna() == False]
-    in_array = lambda value, column: df[df.apply(lambda x: value in x[column], axis=1) == True]
-    df_top_five = top_five(df, column, split_by)[column]
-    df0 = count_by_year(in_array(df_top_five[0], column))
-    df1 = count_by_year(in_array(df_top_five[1], column))
-    df2 = count_by_year(in_array(df_top_five[2], column))
-    df3 = count_by_year(in_array(df_top_five[3], column))
-    df4 = count_by_year(in_array(df_top_five[4], column))
+    df = df[['citation_publication_date', column]]
+    df = df.explode(column)
+    df['year'] = df.apply(lambda x: x['citation_publication_date'][:4], axis=1)
+    df = df[df['year'] != 'None']
+    df['year'] = df['year'].astype(int)
+    df = df[['year', column]]
+    top_five = df.groupby(column).size().to_frame().reset_index().sort_values(by=0, ascending=False)
+    top_five = top_five.reset_index(drop=True)[:5][column].to_list()
+    df = df[df[column].isin(top_five)]
+    dfs = [df[df[column] == x].groupby('year').size() for x in top_five]
+    dfs = [x.to_frame().reset_index().rename(columns={0: 'count'}) for x in dfs]
     
     # visualize
     fig, ax = plt.subplots(figsize=(15, 5))
-    plt.plot('year', 'count', data=df0, color='black')
-    plt.plot('year', 'count', '--', data=df1, color='red')
-    plt.plot('year', 'count', data=df2, color='blue')
-    plt.plot('year', 'count', '--', data=df3, color='green')
-    plt.plot('year', 'count', data=df4, color='orange')
+    plt.plot('year', 'count', data=dfs[0], color='black')
+    plt.plot('year', 'count', '--', data=dfs[1], color='red')
+    plt.plot('year', 'count', data=dfs[2], color='blue')
+    plt.plot('year', 'count', '--', data=dfs[3], color='green')
+    plt.plot('year', 'count', data=dfs[4], color='orange')
     plt.title(title)
     plt.xlabel('Year')
     plt.ylabel('Total paper')
-    ax.legend(labels=[df_top_five[0], df_top_five[1], df_top_five[2], df_top_five[3], df_top_five[4]])
+    plt.xticks(np.arange(df.year.min(), df.year.max()+1, 3))
+    ax.legend(labels=top_five)
     plt.show()
